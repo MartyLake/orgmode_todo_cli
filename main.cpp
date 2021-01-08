5f7435048337253f65993a0c1e243fd62297ba94
Matthieu Ubuntu <matthieu@talbot.audio>
2021-01-08 01:33:43 +0100

    Cleanup, better formatting, credit copied code.

--- main.cpp
+++ main.cpp
@@ -3,28 +3,29 @@
 #include <fstream>
 #include <iostream>
 #include <locale>
+#include <regex>
 #include <sstream>
 #include <string>
 #include <vector>
 
 using namespace std;
-
-// trim from start (in place)
-static inline void ltrim(std::string &s) {
+// string utils, from
+// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+inline void ltrim(std::string &s) {
   s.erase(s.begin(), std::find_if(s.begin(), s.end(), [](unsigned char ch) {
             return !std::isspace(ch) && ch != '\n';
           }));
 }
-
-// trim from end (in place)
-static inline void rtrim(std::string &s) {
+inline void rtrim(std::string &s) {
   s.erase(std::find_if(
               s.rbegin(), s.rend(),
               [](unsigned char ch) { return !std::isspace(ch) && ch != '\n'; })
               .base(),
           s.end());
 }
-
+// end copy of
+// https://stackoverflow.com/questions/216823/whats-the-best-way-to-trim-stdstring
+// ansi colors
 inline string c_black(string s) { return "\033[30m" + s + "\033[39m"; }
 inline string c_red(string s) { return "\033[31m" + s + "\033[39m"; }
 inline string c_green(string s) { return "\033[32m" + s + "\033[39m"; }
@@ -35,13 +36,12 @@ inline string c_cyan(string s) { return "\033[36m" + s + "\033[39m"; }
 inline string c_white(string s) { return "\033[37m" + s + "\033[39m"; }
 inline string c_bold(string s) { return "\033[1m" + s + "\033[22m"; }
 inline string c_half_bright(string s) { return "\033[2m" + s + "\033[22m"; }
-
+// tokenizer
 struct Token {
   enum Type { literal, tree_node_nesting_value, todo, next, done } type;
   string str_value;
   size_t int_value;
 };
-
 vector<Token> tokenize(istream &s) {
   vector<Token> tokens;
   string line;
@@ -84,7 +84,7 @@ vector<Token> tokenize(istream &s) {
   }
   return tokens;
 }
-
+// parser
 struct Leaf {
   enum State { _, TODO, NEXT, DONE } state{_};
   string title{};
@@ -93,11 +93,10 @@ struct Leaf {
   bool has_TODO_child{false};
   bool has_NEXT_child{false};
 };
-
 string to_string(Leaf::State s) {
   switch (s) {
   case Leaf::State::_:
-    return "";
+    return "    ";
   case Leaf::State::TODO:
     return "TODO";
   case Leaf::State::NEXT:
@@ -108,7 +107,6 @@ string to_string(Leaf::State s) {
   assert("unimplemented");
   return "UNIMPLEMENTED";
 }
-
 vector<Leaf> parse_tree(const vector<Token> &tokens, const bool compact) {
   vector<Leaf> children;
   {
@@ -135,16 +133,12 @@ vector<Leaf> parse_tree(const vector<Token> &tokens, const bool compact) {
   bool parsing_title{true};
   for (const auto &t : tokens) {
     Leaf &current = children.back();
-    // cout << "///" << endl;
     if (t.type == Token::literal) {
       if (t.str_value.empty()) {
         parsing_title = false;
         continue;
       }
       string s = parsing_title ? current.title : current.str;
-      // cout << "current = " << s << ", with size " << s.size() << endl;
-      // cout << "t.s = " << t.str_value << ", with size" << t.str_value.size()
-      // << endl;
       if (!s.empty()) {
         s += parsing_title ? " " : "\n";
       }
@@ -154,7 +148,6 @@ vector<Leaf> parse_tree(const vector<Token> &tokens, const bool compact) {
       } else {
         current.str = s;
       }
-      // cout << "current = " << current.str << endl;
     } else if (t.type == Token::todo) {
       current.state = Leaf::TODO;
       mark_parents(Leaf::TODO);
@@ -164,7 +157,7 @@ vector<Leaf> parse_tree(const vector<Token> &tokens, const bool compact) {
     } else if (t.type == Token::done) {
       current.state = Leaf::DONE;
     } else if (t.type == Token::tree_node_nesting_value) {
-      if (compact && !current.str.empty()) {
+      if (!current.str.empty()) {
         rtrim(current.str);
         ltrim(current.str);
       }
@@ -172,20 +165,18 @@ vector<Leaf> parse_tree(const vector<Token> &tokens, const bool compact) {
       l.level = t.int_value;
       children.push_back(l);
       parsing_title = true;
-      // cout << "NEW LEAF:" << l.level << endl;
     }
   }
   return children;
 }
-
+// main
 void display_help(string_view executable_name) {
   cout << "Usage:" << endl;
-  cout << executable_name << " input.org TODO" << endl;
+  cout << executable_name << " TODO input.org" << endl;
   cout << '\t' << " to get all the TODOs" << endl;
-  cout << executable_name << " input.org NEXT" << endl;
-  cout << '\t' << " to get all the NEXTs" << endl;
+  cout << executable_name << " NEXT --compact input.org" << endl;
+  cout << '\t' << " to get all the NEXTs, in compact form" << endl;
 }
-
 int main(int argc, char **argv) {
   vector<string> arguments(argv, argv + argc);
   if (arguments.size() < 3) {
@@ -203,47 +194,44 @@ int main(int argc, char **argv) {
       continue;
     }
   }
-  if (arguments[2] != "TODO" && arguments[2] != "NEXT") {
-    cout << "Unknown state tag:'" << arguments[2] << "'" << endl;
+  if (arguments[1] != "TODO" && arguments[1] != "NEXT") {
+    cout << "Unknown state tag:'" << arguments[1] << "'" << endl;
     display_help(arguments.front());
     return -1;
   }
-  ifstream file(arguments[1]);
+  ifstream file(arguments.back());
   const auto tokens = tokenize(file);
   const auto tree = parse_tree(tokens, compact);
 
   size_t max_level = 0;
   for (const auto &t : tree) {
-    if ((arguments[2] == "TODO" && t.has_TODO_child) ||
-        (arguments[2] == "NEXT" && t.has_NEXT_child)) {
+    if ((arguments[1] == "TODO" && t.has_TODO_child) ||
+        (arguments[1] == "NEXT" && t.has_NEXT_child)) {
       max_level = max(max_level, t.level);
     }
   }
   for (const auto &t : tree) {
-    if ((arguments[2] == "TODO" && t.has_TODO_child) ||
-        (arguments[2] == "NEXT" && t.has_NEXT_child)) {
-      if (t.level == 1 && !compact) {
-        cout << endl;
-      }
+    if ((arguments[1] == "TODO" && t.has_TODO_child) ||
+        (arguments[1] == "NEXT" && t.has_NEXT_child)) {
       if (t.level > 0) {
-        cout << c_half_bright(string(t.level - 1, '*')) << c_bold({"*"})
+        cout << c_half_bright(string(t.level - 1, '|')) << c_bold({"*"})
              << c_blue(c_half_bright(string(max_level - t.level, '.'))) << " ";
       }
-      if (t.state != Leaf::State::_) {
-        if (t.state == Leaf::State::DONE) {
-          cout << c_bold(c_blue(to_string(t.state))) << " ";
-        } else {
-          cout << c_bold(c_green(to_string(t.state))) << " ";
-        }
+      if (t.state == Leaf::State::DONE) {
+        cout << c_bold(c_blue(to_string(t.state))) << " ";
+      } else {
+        cout << c_bold(c_green(to_string(t.state))) << " ";
       }
       cout << t.title;
-      if (!compact) {
-        cout << endl << c_half_bright(t.str);
+      if (!compact && !t.str.empty()) {
+        const string filler = string(t.level - 1, '|') + "." +
+                              string(max_level - t.level, ' ') + " " + "     ";
+        cout << endl
+             << c_half_bright(c_blue(filler))
+             << c_half_bright(
+                    regex_replace(t.str, regex{"\n"}, "\n" + c_blue(filler)));
       }
       cout << endl;
-      if (!compact) {
-        cout << endl;
-      }
     }
   }
   return 0;
